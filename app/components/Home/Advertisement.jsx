82c72ad40db64355f0563f5ce3702f923e6547ae
RobiMankhinStart <mankhinrobi@gmail.com>
2025-09-27 10:54:25 +0600

    Advertisement normal design done. need to do responsive

---
+++ app/components/Home/Advertisement.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+const Advertisement = () => {
+  return (
+    <div>
+      {/* <div className="container"> */}
+      <div className="addRow flex justify-between">
+        <div className="left flex flex-col">
+          <div className="top flex items-center">
+            <img
+              className="w-[360px] h-auto"
+              src="/PlayStation.png"
+              alt="PlayStation"
+            />
+            {/* <Image
+                width={340}
+                height={320}
+                alt="playstation"
+                src={"/PlayStation.png"}
+              /> */}
+            <div className="flex flex-col w-[338px]">
+              <h2 className="text-[49px] font-medium font-inter">
+                Playstation 5
+              </h2>
+              <p className="text-[#909090] text-[14px] font-inter font-medium">
+                Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O
+                will redefine your PlayStation experience.
+              </p>
+            </div>
+          </div>
+          <div className="bot flex justify-between w-[780px] ">
+            <div className="flex gap-12 items-center">
+              <img
+                className="w-[104px]"
+                src="/hero__gnfk5g59t0qe_xlarge_2x 1.png"
+                alt="head phone"
+              />
+              <div className="w-[160px]">
+                <h3 className="font-inter font-light text-[29px]">
+                  Apple AirPods <span className="font-medium">Max</span>
+                </h3>
+                <p className="text-[14px] font-medium font-inter text-[#909090]">
+                  Computational audio. Listen, it's powerful
+                </p>
+              </div>
+            </div>
+            <div className="flex w-[390px] gap-4 items-center bg-[#353535] ">
+              <img
+                className="w-[136px] h-[150px]"
+                src="/image 36.png"
+                alt="vision"
+              />
+              <div className="w-[160px] ">
+                <h2 className="text-[29px] font-light font-inter text-white">
+                  Apple Vision <span className="font-medium">Pro</span>
+                </h2>
+                <p className="text-[14px] font-medium font-inter text-[#909090]">
+                  An immersive way to experience entertainment
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="right flex justify-between items-center pl-[56px] bg-[#D2D2DA]">
+          <div className="w-[360px]">
+            <h3 className="text-[64px] font-inter font-thin">
+              Macbook <span className="font-medium">Air</span>{" "}
+            </h3>
+            <p className="text-[#909090] text-[14px] font-inter font-medium">
+              The new 15‑inch MacBook Air makes room for more of what you love
+              with a spacious Liquid Retina display.
+            </p>
+            <Link
+              className=" inline-block border rounded-md text-[16px] font-medium font-inter w[191px] py-4 px-[56px]"
+              href={"/shop"}
+            >
+              Shop Now
+            </Link>
+          </div>
+          <div>
+            <img
+              className="w-[292px]"
+              src="/MacBook Pro 14.png"
+              alt="MacBook Pro"
+            />
+          </div>
+        </div>
+      </div>
+      {/* </div> */}
+    </div>
+  );
+};
+
+export default Advertisement;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Link from "next/link";
-import Banner from "./components/Banner";
+import Banner from "./components/Home/Banner";
+import Advertisement from "./components/Home/Advertisement";
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <Advertisement />
     </div>
   );
 }
