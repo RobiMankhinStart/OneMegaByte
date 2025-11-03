2fa3a19aa0b9899f32d4753f7f6503fdbd91277c
Robi Mankhin <mankhinrobi@gmail.com>
2025-11-03 10:22:29 +0600

    layout space minus

--- app/(pages)/(auth)/layout.js
+++ app/(pages)/(auth)/layout.js
@@ -1,26 +1,20 @@
-
 import "../../../app/globals.css";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-
 export default async function RootLayout({ children }) {
-   const token = await cookies()
-    const myToken = token.get("token")?.value;
+  const token = await cookies();
+  const myToken = token.get("token")?.value;
 
-console.log(myToken)
-    if(myToken){
-        return redirect("/")
-    }
+  console.log(myToken);
+  if (myToken) {
+    return redirect("/");
+  }
 
   return (
     <html lang="en">
-      <body
-    
-      >
-    
+      <body>
         <div className="mt-28">{children}</div>
-     
       </body>
     </html>
   );
